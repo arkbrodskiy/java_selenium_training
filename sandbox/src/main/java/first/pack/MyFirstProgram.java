e971eddd8aff8fa704b8a73018d6dced50642c1a
Arkadiy Brodskiy <arkbrod@gmail.com>
2017-09-15 20:51:50 -0400

    Задание 2.2. Создана функция distance().
    Функция distance() вычисляет расстояние между двумя точками.

--- sandbox/src/main/java/first/pack/MyFirstProgram.java
+++ sandbox/src/main/java/first/pack/MyFirstProgram.java
@@ -4,4 +4,10 @@ public class MyFirstProgram{
 	public static void main(String[] args){
 		System.out.println("Hello, World!");
 	}
+
+	public static double distance(Point p1, Point p2){
+	    double xDiff = p1.x - p2.x;
+        double yDiff = p1.y - p2.y;
+		return Math.sqrt(xDiff * xDiff + yDiff * yDiff);
+	}
 }
