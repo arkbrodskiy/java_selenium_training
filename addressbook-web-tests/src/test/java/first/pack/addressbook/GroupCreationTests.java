f6ed1abfe711688633e0ad349bd21fed28f90faf
Arkadiy Brodskiy <arkbrod@gmail.com>
2017-09-23 21:19:53 -0400

    Задание 4.4. В модуль перенесен записанный рекордером тест (класс ContactCreationTests). Тест компилируется и работает.

--- addressbook-web-tests/src/test/java/first/pack/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/first/pack/addressbook/GroupCreationTests.java
@@ -10,7 +10,7 @@ import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-public class GroupcreationTests {
+public class GroupCreationTests {
     FirefoxDriver wd;
     
     @BeforeMethod
