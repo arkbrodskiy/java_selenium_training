77bcea843718fc6108b6964d1ce4df35d4fb3abd
Arkadiy Brodskiy <arkbrod@gmail.com>
2017-09-24 20:12:18 -0400

    Задание 6.1. Создан класс GroupModificationTests и в нем записан тестовый метод testGroupModification(). Добавлены недостающие методы в класс GroupHelper.

---
+++ addressbook-web-tests/src/test/java/first/pack/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,17 @@
+package first.pack.addressbook.tests;
+
+import first.pack.addressbook.model.GroupData;
+import org.testng.annotations.Test;
+
+public class GroupModificationTests extends TestBase {
+
+    @Test
+    public void testGroupModification(){
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().initGroupModification();
+        app.getGroupHelper().fillGroupForm(new GroupData("Edited group 01", "Edited group 01 header", "Edited group 06 footer"));
+        app.getGroupHelper().submitGroupModification();
+        app.getGroupHelper().returnToGroupPage();
+    }
+}
