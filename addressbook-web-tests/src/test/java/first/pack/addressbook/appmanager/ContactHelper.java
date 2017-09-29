6b529ec37504f13e1fffb92d904664ecfd643487
Arkadiy Brodskiy <arkbrod@gmail.com>
2017-09-28 22:00:39 -0400

    Задание 7. Реализована возможность запуска тестов в трех боаузерах.
    Исполняемые файлы для драйверов Chrome и Internet Explorer размещены в папке, путь к которой прописан в переменной PATH. Поэтому в репозиторий они не попали.

--- addressbook-web-tests/src/test/java/first/pack/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/first/pack/addressbook/appmanager/ContactHelper.java
@@ -2,11 +2,12 @@ package first.pack.addressbook.appmanager;
 
 import first.pack.addressbook.model.ContactData;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class ContactHelper extends HelperBase {
 
-    public ContactHelper(FirefoxDriver wd) {
+    public ContactHelper(WebDriver wd) {
         super(wd);
     }
 
