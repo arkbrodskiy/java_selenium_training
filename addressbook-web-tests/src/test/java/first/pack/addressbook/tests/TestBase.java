5d2591d582caa0a5f014260aa71039cf5a0602be
Arkadiy Brodskiy <arkbrod@gmail.com>
2017-10-12 21:38:38 -0400

    Задание 10.1. Реализован запуск одного браузера для всего набора тестов.

--- addressbook-web-tests/src/test/java/first/pack/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/first/pack/addressbook/tests/TestBase.java
@@ -1,26 +1,20 @@
 package first.pack.addressbook.tests;
 
 import first.pack.addressbook.appmanager.ApplicationManager;
-import first.pack.addressbook.model.ContactData;
-import first.pack.addressbook.model.GroupData;
 import org.openqa.selenium.remote.BrowserType;
-import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-
-import java.util.Comparator;
-import java.util.List;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+    protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
-    @BeforeMethod
+    @BeforeSuite
     public void setUp() throws Exception {
         app.init();
     }
 
-    @AfterMethod
+    @AfterSuite
     public void tearDown() {
         app.stop();
     }
