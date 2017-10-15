9d6760b89a12c446c2f7e281d9ee671502f5787c
Arkadiy Brodskiy <arkbrod@gmail.com>
2017-10-15 17:56:23 -0400

    Задание 11.3. Добавлен негативный тест, проверяющий невозможность создания контакта с именем, содержащим апостроф.

--- addressbook-web-tests/src/test/java/first/pack/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/first/pack/addressbook/tests/ContactCreationTests.java
@@ -28,4 +28,23 @@ public class ContactCreationTests extends TestBase{
                 before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
     }
 
+    @Test
+    public void testBadContactCreation() {
+        Contacts before = app.contact().takeAll();
+        ContactData contact = new ContactData()
+                .withFirstName("XXДаже'")
+                .withLastName("XXСказка'")
+                .withNickname("Спать")
+                .withTitle("Ложится")
+                .withCompany("Чтобы")
+                .withHomePhone("4369852147")
+                .withMobilePhone("1652058741")
+                .withOfficePhone("9696323258")
+                .withEmail("dnri@fhjdgt.so");
+        app.contact().create(contact);
+        assertThat(app.contact().count(), equalTo(before.size()));
+        Contacts after = app.contact().takeAll();
+        assertThat(after, equalTo(before));
+    }
+
 }
