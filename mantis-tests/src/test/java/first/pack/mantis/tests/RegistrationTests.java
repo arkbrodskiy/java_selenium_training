c35966c5f8a51f78f9d32eb0f985200cb523b14b
Arkadiy Brodskiy <arkbrod@gmail.com>
2017-11-04 09:00:24 -0400

    Задание 18.02. Реализована ленивая инициализация браузера.

---
+++ mantis-tests/src/test/java/first/pack/mantis/tests/RegistrationTests.java
@@ -0,0 +1,11 @@
+package first.pack.mantis.tests;
+
+import org.testng.annotations.Test;
+
+public class RegistrationTests extends TestBase {
+
+    @Test
+    public void testRegistration(){
+        app.registration().start("user1", "e@mail.yes");
+    }
+}
